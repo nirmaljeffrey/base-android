6aa0c6647449689e71593e0041c77b00216018b6
Nirmal Jeffrey <jeffattry@gmail.com>
2021-12-09 12:25:58 +0530

    feat: Add networkBoundResource.kt for offline caching

---
+++ app/src/main/java/io/github/geeksforgeinc/base_android/utils/networkBoundResource.kt
@@ -0,0 +1,29 @@
+package io.github.geeksforgeinc.base_android.utils
+
+import kotlinx.coroutines.flow.*
+import timber.log.Timber
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }
+) = flow {
+    val data = query().first()
+
+    val flow = if (shouldFetch(data)) {
+        emit(Resource.Loading(data))
+
+        try {
+            saveFetchResult(fetch())
+            query().map { Resource.Success(it) }
+        } catch (throwable: Throwable) {
+            Timber.e(throwable)
+            query().map { Resource.Error(throwable, it) }
+        }
+    } else {
+        query().map { Resource.Success(it) }
+    }
+
+    emitAll(flow)
+}
